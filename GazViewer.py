833ecc8f3d2830e6f88283afdc471c556d143293
mccarthyg <mccarthyg@UKLONLJSR01J3>
2023-05-04 13:32:09 +0100

    V1

---
+++ GazViewer.py
@@ -0,0 +1,68 @@
+import streamlit as st
+from tools import ifchelper
+import json
+import ifcopenshell
+##################### STREAMLIT IFC-JS COMPONENT MAGIC ######################
+from pathlib import Path  #
+from re import L  #
+from typing import Optional  #
+import streamlit.components.v1 as components  #
+
+#                                                                           #
+#                                                                           #
+# Tell streamlit that there is a component called ifc_js_viewer,            #
+# and that the code to display that component is in the "frontend" folder   #
+frontend_dir = 'pages/frontend-viewer'
+_component_func = components.declare_component(  #
+    "ifc_js_viewer", path=str(frontend_dir)  #
+)  #
+
+#                                                                           #
+# Create the python function that will be called                            #
+def ifc_js_viewer(url: Optional[str] = None):
+    component_value = _component_func(url=url)
+    return component_value
+
+#                                                                           #
+#############################################################################
+
+def callback_upload():
+    session["file_name"] = session["uploaded_file"].name
+    session["array_buffer"] = session["uploaded_file"].getvalue()
+    session["ifc_file"] = ifcopenshell.file.from_string(session["array_buffer"].decode("utf-8"))
+    session["is_file_loaded"] = True
+
+    ### Empty Previous Model Data from Session State
+    #session["isHealthDataLoaded"] = False
+    #session["HealthData"] = {}
+    #session["Graphs"] = {}
+    #session["SequenceData"] = {}
+    #session["CostScheduleData"] = {}
+
+    ### Empty Previous DataFrame from Session State
+    #session["DataFrame"] = None
+    #session["Classes"] = []
+    #session["IsDataFrameLoaded"] = False
+
+def get_current_ifc_file():
+    return session.array_buffer
+
+def draw_3d_viewer():
+    session.ifc_js_response = ifc_js_viewer(get_current_ifc_file())
+    st.sidebar.success("Visualiser loaded")
+
+def execute():
+    if "ifc_file" in session and session["ifc_file"]:
+        if "ifc_js_response" not in session:
+            session["ifc_js_response"] = ""
+        draw_3d_viewer()
+    else:
+        st.header("Step 1: Load a file from the Home Page")
+
+st.sidebar.file_uploader("Choose a file", type=['ifc'], key="uploaded_file", on_change=callback_upload)
+
+session = st.session_state
+
+#ifc_file = ifcopenshell.file.from_string('IFC/FLB-ACM-XX-ZZ-M3-AR-000001_S0_P01.ifc')
+
+ifc_js_viewer(get_current_ifc_file())
